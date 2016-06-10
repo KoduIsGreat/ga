525eb961aa2b20059bb5d5df0f613252cd9225c5
Adam <aashelt90@gmail.com>
2016-06-09 22:34:08 -0400

    updated git ignore added binaryIntTransformer

---
+++ src/org/aas/ga/trans/BinaryIntTransformer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.aas.ga.trans;
+
+import org.aas.ga.genes.BinaryGene;
+import org.aas.ga.genes.Gene;
+
+/**
+ *
+ * @author Adam
+ */
+public class BinaryIntTransformer extends BaseTransformer<Integer>
+{
+
+    @Override
+    public Integer transformGene(Gene gene)
+    {
+        BinaryGene bg = (BinaryGene) gene;
+        int val =0;
+        for (Integer i : bg.getDna())
+            val += Math.pow(2.0*bg.getDna().get(i), bg.getDna().size()-i);
+        return val;
+    }
+    
+}
